ffcf75b94c2efc4bc067de3d3076d91aefb42088
Akbar Hazura <akbarhazura@gmail.com>
2021-09-14 04:09:59 +0700

    set up

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Product extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Ini Halaman Product.js</h3>
+            </div>
+        )
+    }
+}
